a8ce15c1b20a0044d37d14d1ab7e24348df68d78
Prateek Singh <prateekraj2602@gmail.com>
2022-09-11 14:21:07 +0530

    Added database

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const date = require(__dirname + "/date.js"); //locale self-made module
 
+const mongoose = require("mongoose");
+
+
 const app = express();
 
 app.use(express.static("public")); //Serving static files in Express
@@ -10,31 +13,70 @@ app.use(express.urlencoded({extended: true}));  // replaces bodyparser
 
 app.set("view engine", "ejs");
 
-var items = ["Eat", "Read", "Workout"];
-var workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+
 var day = "";
+
+
+////// Creating out Item schema
+const itemsSchema = {
+  name : String
+};
+
+////// Creating the model
+const Item = mongoose.model("Item", itemsSchema);
+
+////// Creating the documents
+const item1 = new Item({
+  name : "work1"
+});
+const item2 = new Item({
+  name : "work2"
+});
+const item3 = new Item({
+  name : "work3"
+});
+
+
+const defaultItems = [item1, item2, item3];
 
 app.get("/", function(req, res){
   let day = date.getDate();
-  res.render("list.ejs", {listName: day, newListItems: items});
+
+  Item.find({}, function(err, foundItems){
+
+    if(foundItems.length === 0){
+      Item.insertMany(defaultItems, function(err){
+        if(err)console.log(err);
+        else console.log("Successfully saved defaultItems to DB.");
+
+        res.redirect("/");                  
+      })
+    }else res.render("list.ejs", {listName: day, newListItems: foundItems});
+  });
 
 })
 
 app.post("/", function(req, res){
 
-  //console.log(req.body);
+  const itemName = req.body.newItem;
+  const item = new Item({
+    name : itemName
+  });
+  item.save();
 
-  if(req.body.button === "Work List"){
-    workItems.push(req.body.newItem);
-    res.redirect("/work");
-  }else {
-    items.push(req.body.newItem);
-    res.redirect("/");
-  }
+  res.redirect("/");
+
 })
 
-app.get("/work", function(req, res){
-  res.render("list.ejs", {listName: "Work List", newListItems: workItems});
+app.post("/delete", function(req, res){
+  const checkedItemId = req.body.checkboxItem;
+  //console.log(checkedItemId);
+  Item.findByIdAndRemove(checkedItemId, function(err) {
+    if(!err)console.log("Successfully deleted checked item.");
+    res.redirect("/");
+  })
 })
 
 app.listen(3000, function(){
